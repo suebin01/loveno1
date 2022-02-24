0b5cdf08f0b13809b1b9a1d71caeedaa596187c4
Suebin Park <soobpark@naver.com>
2022-02-25 02:02:04 +0900

    login > localStorage

--- js/login.js
+++ js/login.js
@@ -1,2 +1,14 @@
-const userName = document.querySelector(".login-form__name");
-const userTemp = document.querySelector(".login-form__temp");
+const loginForm = document.querySelector(".login-form");
+const loginInputName = document.querySelector(".login-form__name");
+const loginInputTemp = document.querySelector(".login-form__temp");
+
+const USERNAME_KEY = "username";
+const USERTEMP_KEY = "temperature";
+
+function onLoginSubmit() {
+  const userName = loginInputName.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  const userTemp = loginInputTemp.value;
+  localStorage.setItem(USERTEMP_KEY, userTemp);
+}
+loginForm.addEventListener("submit", onLoginSubmit);
