85285a81a9a6243e3e4bce9f1da3f0dd88f7a80d
Suebin Park <soobpark@naver.com>
2022-02-26 03:30:09 +0900

    logout button

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,15 @@
 const popupScreen = document.querySelector(".popup-bgr");
 const checkPopup = document.querySelector(".ok");
+const cancelPopup = document.querySelector(".cancel");
 
 function removeBox() {
   popupScreen.style.display = "none";
 }
+function exitPage() {
+  cancelPopup.setAttribute("href", "index.html");
+  localStorage.removeItem("username");
+  localStorage.removeItem("temperature");
+}
 
 checkPopup.addEventListener("click", removeBox);
+cancelPopup.addEventListener("click", exitPage);
